20c3f682f82c1ec82f47deca9529c46a6edb1921
Marcello DeSales <marcello.desales@gmail.com>
2023-02-23 19:12:05 -0800

    :crab: :recycle: :module: update project module v2
    It now uses abstractions of tuples, matches, etc.

--- src/main.rs
+++ src/main.rs
@@ -77,71 +77,6 @@ fn variable_scope_and_shadowing() {
     let a: f32 = 5.6;
 }
 
-fn calculate_distance_between_airports_v2() {
-    const EARTH_RADIOS_IN_KILOMETERS: f64 = 6371.0;
-
-    // array of routes
-    let route = [
-        ("KCLE", 41.4075, -81.851111),
-        ("LEYIR", 41.51030, -83.88080),
-        ("PIONS", 41.65390, -84.48190),
-        ("ZOSER", 41.72390, -84.78130),
-        ("MODEM", 41.72800, -84.89730),
-        ("BRYTO", 41.74170, -85.31320),
-        ("SEWTO", 41.74780, -85.51130),
-        ("GIJ", 41.76860, -81.31850),
-        ("NEPTS", 41.96750, -87.05300),
-        ("THORR", 42.12330, -87.60030),
-        ("OBK", 42.22140, -87.95160),
-        ("COTON", 42.31990, -89.31220),
-        ("DBQ", 41.40150, -89.31220),
-        ("VIGGR", 42.555220, -93.12410),
-        ("FOD", 42.61110, -94.29480),
-        ("ONL", 42.47050, -98.68690),
-        ("BFF", 42.89420, -103.48200),
-        ("OCS", 41.59020, -109.01500),
-        ("PUDVY", 41.54270, -109.34200),
-        ("WEGEM", 41.44560, -109.99000),
-        ("KSLC", 40.7861, -111.9822)
-    ];
-
-    let mut total_distance :f64 = 0.0;
-
-    // Options is the way to implement generics
-    // Options can declare data types
-    let mut previous_waypoint: Option<(&str, f64, f64)> = None;
-
-    for waypoint in route.iter() {
-        match previous_waypoint {
-            None => {
-                previous_waypoint = Option::from(waypoint.clone());
-                continue;
-            }
-            Some(previous_value) => {
-                let previous_waypoint_radians = previous_value.1.to_radians();
-                let waypoint_radians = waypoint.1.to_radians();
-
-                let delta_lat = (previous_value.1 - waypoint.1).to_radians();
-                let delta_lon = (previous_value.2 - waypoint.2).to_radians();
-
-                let inner_central_angle = f64::powi((delta_lat / 2.0).sin(), 2)
-                    + previous_waypoint_radians.cos()
-                    * waypoint_radians.cos()
-                    * f64::powi((delta_lon / 2.0).sin(), 2);
-
-                let central_angle = 2.0 * inner_central_angle.sqrt().asin();
-                let distance = EARTH_RADIOS_IN_KILOMETERS * central_angle;
-                total_distance += distance;
-
-                // set the current as previous
-                previous_waypoint = Option::from(waypoint.clone());
-                println!("The distance between '{}' and '{}' is '{:.1}' kolometers",
-                         previous_value.0, waypoint.0, distance);
-            }
-        }
-    }
-}
-
 fn ownership_and_borrowing() {
     // at this point, the string is at the heap
     let original = String::from("original value");
@@ -951,7 +886,7 @@ fn main() {
     control_flow::ControlFlow::if_let_compiler_macro();
     control_flow::ControlFlow::while_for_loop_loops();
 
-    calculate_distance_between_airports_v2();
+    project::ProjectV2::calculate_distance_between_airports();
 
     ownership_and_borrowing();
 
--- src/project.rs
+++ src/project.rs
@@ -28,3 +28,71 @@ pub mod ProjectV1 {
         println!("The distance between two points is {:.1} kolometers", distance);
     }
 }
+
+pub mod ProjectV2 {
+
+    pub fn calculate_distance_between_airports() {
+        const EARTH_RADIOS_IN_KILOMETERS: f64 = 6371.0;
+
+        // array of routes
+        let route = [
+            ("KCLE", 41.4075, -81.851111),
+            ("LEYIR", 41.51030, -83.88080),
+            ("PIONS", 41.65390, -84.48190),
+            ("ZOSER", 41.72390, -84.78130),
+            ("MODEM", 41.72800, -84.89730),
+            ("BRYTO", 41.74170, -85.31320),
+            ("SEWTO", 41.74780, -85.51130),
+            ("GIJ", 41.76860, -81.31850),
+            ("NEPTS", 41.96750, -87.05300),
+            ("THORR", 42.12330, -87.60030),
+            ("OBK", 42.22140, -87.95160),
+            ("COTON", 42.31990, -89.31220),
+            ("DBQ", 41.40150, -89.31220),
+            ("VIGGR", 42.555220, -93.12410),
+            ("FOD", 42.61110, -94.29480),
+            ("ONL", 42.47050, -98.68690),
+            ("BFF", 42.89420, -103.48200),
+            ("OCS", 41.59020, -109.01500),
+            ("PUDVY", 41.54270, -109.34200),
+            ("WEGEM", 41.44560, -109.99000),
+            ("KSLC", 40.7861, -111.9822)
+        ];
+
+        let mut total_distance :f64 = 0.0;
+
+        // Options is the way to implement generics
+        // Options can declare data types
+        let mut previous_waypoint: Option<(&str, f64, f64)> = None;
+
+        for waypoint in route.iter() {
+            match previous_waypoint {
+                None => {
+                    previous_waypoint = Option::from(waypoint.clone());
+                    continue;
+                }
+                Some(previous_value) => {
+                    let previous_waypoint_radians = previous_value.1.to_radians();
+                    let waypoint_radians = waypoint.1.to_radians();
+
+                    let delta_lat = (previous_value.1 - waypoint.1).to_radians();
+                    let delta_lon = (previous_value.2 - waypoint.2).to_radians();
+
+                    let inner_central_angle = f64::powi((delta_lat / 2.0).sin(), 2)
+                        + previous_waypoint_radians.cos()
+                        * waypoint_radians.cos()
+                        * f64::powi((delta_lon / 2.0).sin(), 2);
+
+                    let central_angle = 2.0 * inner_central_angle.sqrt().asin();
+                    let distance = EARTH_RADIOS_IN_KILOMETERS * central_angle;
+                    total_distance += distance;
+
+                    // set the current as previous
+                    previous_waypoint = Option::from(waypoint.clone());
+                    println!("The distance between '{}' and '{}' is '{:.1}' kolometers",
+                             previous_value.0, waypoint.0, distance);
+                }
+            }
+        }
+    }
+}
