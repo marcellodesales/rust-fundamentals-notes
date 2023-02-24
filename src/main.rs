de704d0d6127f5d42501972aa3d8bd231f3c3104
Marcello DeSales <marcello.desales@gmail.com>
2023-02-23 19:22:42 -0800

    :crab: :package: add module functions definitions

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,7 @@ use crate::language_operators::LanguageOperators;
 pub mod project;
 pub mod control_flow;
 pub mod ownership_and_borrowing;
+pub mod functions_definitions;
 
 fn variable_scope_and_shadowing() {
     let scope_test = "outer scope";
@@ -76,71 +77,6 @@ fn variable_scope_and_shadowing() {
     // changing the type of a variable in the same scope
     let a = "String";
     let a: f32 = 5.6;
-}
-
-// param types, and return type with ->, can be omitted if not returning
-// return can be omitted and without ;
-fn return_greater(first: u8, second: u8) -> u8 {
-    if first >= second {
-        first
-    } else {
-        second
-    }
-}
-
-fn calling_functions() {
-    // Pass by Value is when the & is not provided, it's a copy of the value in the stack
-    let greater = return_greater(10, 5);
-    println!("{}", greater);
-
-    // Pass by reference is passing the pointer
-    // borrowing as part of functions
-    let mut original = String::from("Mutable Original");
-    println!("Outter Original: \t'{}'", original);
-
-    {
-        print_original(&original);
-        change_original(&mut original);
-        println!("inner scope of original: \t'{}'", original);
-    }
-}
-
-fn closure_methods() {
-    let name = "Duck Airlines";
-    let write_message_closure = || {
-        println!("Hey this is a closure: {}", name);
-    };
-
-    // calling a closure
-    write_message_closure();
-
-    // it goes between the pipes
-    let write_message_closure_with_params = |slogan: String| {
-        println!("closure {}. {}", name, slogan);
-    };
-
-    // calling closure with param
-    write_message_closure_with_params(String::from("We hit the ground every time."));
-
-    let write_message_and_return = |slogan: String| -> String {
-        // since this is the return, it's without the semi-colon ;
-        String::from(format!("{}. {}", name, slogan))
-    };
-    let phrase = write_message_and_return(String::from("Phease: We hit again!!!"));
-    println!("{}", phrase)
-}
-
-// sending the memory address rather than the actual value
-fn print_original(original: &String) {
-    println!("fn print_original: \t'{}'", original);
-}
-
-fn change_original(original: &mut String) {
-    let next = original;
-    *next = String::from("changed value!!!");
-    // The next value MUST MUST MUST be used here as it borrowed the ownership from original pointer
-    // The ownership is changed until the exit of the function, which is the scope of that function
-    println!("fn change_original: \t'{}'", next);
 }
 
 fn unrecoverable_error() {
@@ -743,9 +679,8 @@ fn main() {
     ownership_and_borrowing::OwnershipAndBorrowing::moving_ownership();
     ownership_and_borrowing::OwnershipAndBorrowing::lifetimes_and_dangling_references();
 
-    calling_functions();
-
-    closure_methods();
+    functions_definitions::FunctionsDefinitions::calling_functions();
+    functions_definitions::FunctionsDefinitions::closure_methods();
 
     //unrecoverable_error();
 
